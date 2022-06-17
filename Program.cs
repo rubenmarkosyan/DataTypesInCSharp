41f6f2b3c165508f54d98b8060e4151d7edb5f90
Ruben Markosyan <ruben.markosyan@certipro.com>
2022-06-17 12:29:05 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿{
+    int a = 1_500_000_000;
+    int b = 1_500_000_000;
+
+    int c = a + b;
+
+    Console.WriteLine(c);
+    Console.ReadLine();
+}
+
+{
+    double a = 0.1;
+    double b = 0.2;
+    Console.WriteLine($"{a} + {b} = {a + b}");
+
+    float num1 = 0.1F;
+    float num2 = 0.2F;
+    Console.WriteLine($"{num1} + {num2} = {num1 + num2}");
+
+    a = 17.3;
+    b = 17;
+    Console.WriteLine($"{a} - {b} = {a - b}");
+
+    num1 = 17.3F;
+    num2 = 17F;
+    Console.WriteLine($"{num1} - {num2} = {num1 - num2}");
+    Console.ReadLine();
+
+    //Try the same in JavaScript the browser console 
+    //console.log(0.1 + 0.2);
+    //console.log(17.3 - 17);
+}
+
+
